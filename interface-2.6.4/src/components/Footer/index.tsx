5797dc549a2208a97434b8355f2824bbdbf22990
Sakib <sakib.it@ramaera.in>
2024-05-14 13:10:25 +0530

    footer responsive

--- interface-2.6.4/src/components/Footer/index.tsx
+++ interface-2.6.4/src/components/Footer/index.tsx
@@ -1,73 +1,98 @@
 import { useRef } from 'react'
-import { BookOpen, Code, Info, MessageCircle, PieChart } from 'react-feather'
 import styled from 'styled-components'
 import React from 'react'
-import { ExternalLink } from '../../theme'
 import telegram from '../../assets/logo/telegram.svg'
+import twitter from '../../assets/logo/twitter.svg'
+import discord from '../../assets/logo/discord.svg'
+import docs from '../../assets/logo/docs.svg'
+import about from '../../assets/logo/about.svg'
 
 const StyledFooter = styled.div`
   margin-left: 0.5rem;
+  width: 100%;
   display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+  justify-content: space-between;
+  padding-inline: 3rem;
   border: none;
   text-align: left;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `
 
-const FooterItem = styled(ExternalLink)`
-  flex: 1;
-  padding: 0.5rem 0.5rem;
-  color: ${({ theme }) => theme.text2};
-  :hover {
-    color: ${({ theme }) => theme.text1};
-    cursor: pointer;
-    text-decoration: none;
-  }
-  > svg {
-    margin-right: 8px;
-  }
-`
 const ButtonContainer = styled.div`
   display: flex;
-  justify-content: center; /* Align buttons horizontally at the center */
-  gap: 10px; /* Add some space between buttons */
+  justify-content: flex-end;
+  gap: 25px; /* Add some space between buttons */
 `
 
-const CODE_LINK = 'https://github.com/Uniswap/uniswap-interface'
+const Button = styled.button`
+  background: #e3e1eb;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+  border-radius: 5px;
+  overflow: hidden;
+  width: 45px;
+  height: 45px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background: #8050df;
+  }
+
+  img {
+    width: 70%;
+    height: 70%;
+    object-fit: contain;
+  }
+`
+
+const StyledText = styled.text`
+  font-size: 14px;
+  color: #f9f6f6;
+  font-weight: bold;
+  margin-top: 10px;
+
+  @media (max-width: 768px) {
+    text-align: center;
+    margin: 20px 0;
+  }
+`
 
 export default function Footer() {
   const node = useRef<HTMLDivElement>()
 
   return (
     <StyledFooter ref={node as any}>
-      <div>
-        <FooterItem id="link" href="https://ramascan.com/">
-          <Info size={14} />
-          About
-        </FooterItem>
-        <FooterItem id="link" href="https://docs.ramestta.com/">
-          <BookOpen size={14} />
-          Docs
-        </FooterItem>
-        <FooterItem id="link" href={CODE_LINK}>
-          <Code size={14} />
-          Code
-        </FooterItem>
-        <FooterItem id="link" href="https://discord.gg/EwFs3Pp">
-          <MessageCircle size={14} />
-          Discord
-        </FooterItem>
-        <FooterItem id="link" href="https://uniswap.info/">
-          <PieChart size={14} />
-          Analytics
-        </FooterItem>
-        <ButtonContainer>
-          {/* Example buttons */}
-          <button><img src={telegram} /></button>
-          <button>Button 2</button>
-          <button>Button 3</button>
-        </ButtonContainer>
-      </div>
+      {/* <div> */}
+      <StyledText>© 2024 Ramestta Blockchain . | All rights reserved</StyledText>
+      <ButtonContainer>
+        <Button>
+          <a href="https://ramascan.com/">
+            <img src={about} alt="about" />
+          </a>
+        </Button>
+        <Button>
+          <img src={telegram} />
+        </Button>
+        <Button>
+          <img src={twitter} />
+        </Button>
+        <Button>
+          <a href="https://discord.gg/EwFs3Pp">
+            <img src={discord} alt="Discord" />
+          </a>
+        </Button>
+        <Button>
+          <a href="https://docs.ramestta.com/">
+            <img src={docs} alt="docs" />
+          </a>
+        </Button>
+      </ButtonContainer>
+
+      {/* </div> */}
     </StyledFooter>
   )
 }
